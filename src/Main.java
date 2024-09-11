e1aecf9220180865b654f4d951f779c7de0b69fe
edu30311 <202430311@email.daelim.ac.kr>
2024-09-11 17:22:51 +0900

    처음 커밋

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 5.0;
+        boolean c = true;
+        char d = 1;
+        String e = "abc";
+        System.out.println(b);
+    }
+}
